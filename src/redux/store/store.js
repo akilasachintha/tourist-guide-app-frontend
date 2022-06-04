ea75dde6160b8e8a90a4267a2177aaca2589868c
HPI dimal <84616546+IsuruDimal@users.noreply.github.com>
2022-06-04 10:45:27 +0530

    change files

--- src/redux/store/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import locationsReducer from "./locationsSlice";
-
-export default configureStore({
-  reducer: {
-    locations: locationsReducer,
-  },
-});
